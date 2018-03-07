dd4b0563a1dac35c6b0a461e26e78ee7ac0f19d5
LuddeH98 <ludwig.hansson1@gmail.com>
2018-03-07 19:29:33 +0100

    Added some new routes

--- route/internetbanken.js
+++ route/internetbanken.js
@@ -2,7 +2,7 @@
 
 const express    = require("express");
 const router     = express.Router();
-const skolan2    = require("../src/internetbanken.js");
+const bank    = require("../src/internetbanken.js");
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: false});
 
@@ -12,6 +12,46 @@ router.get("/index", (req, res) => {
     };
 
     res.render("bankIndex/index", data);
+});
+
+router.get("/register", (req, res) => {
+    let data = {
+        title: "Register To Internetbanken"
+    };
+
+    res.render("bankIndex/register", data);
+});
+
+router.get("/login", (req, res) => {
+    let data = {
+        title: "Login to Internetbanken"
+    };
+
+    res.render("bankIndex/login", data);
+});
+
+router.get("/dashboard", (req, res) => {
+    let data = {
+        title: "Dashboard"
+    };
+
+    res.render("bankIndex/dashboard", data);
+});
+
+router.get("/accounts", (req, res) => {
+    let data = {
+        title: "Viewing accounts for user ID"
+    };
+
+    res.render("bankIndex/accounts", data);
+});
+
+router.get("/swish", (req, res) => {
+    let data = {
+        title: "Swish App"
+    };
+
+    res.render("bankIndex/swishapp", data);
 });
 
 module.exports = router;
